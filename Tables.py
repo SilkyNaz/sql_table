a648d4ab8f3f933a37b951f7ea954547d6c686af
Silky <Nazadon504@gmail.com>
2025-03-01 04:35:59 -0500

    - My code for my 2 Pandas tables

--- Tables.py
+++ Tables.py
@@ -1,3 +1,6 @@
+#Nazier Reina
+#Period D
+
 import sqlite3
 import pandas as pd
 
@@ -5,8 +8,12 @@ conn = sqlite3.connect('Rivals.sqlite')
 
 cursor = conn.cursor()
 
+#Pandas table for heroes
+
 df = pd.read_csv("C:/Users/nazad/Downloads/Heros.csv")
 df.to_sql( 'heroes', conn, if_exists='append', index=False)
+
+#pandas table for abilities
 
 df2 = pd.read_csv("C:/Users/nazad/Downloads/Abilities.csv")
 df2.to_sql( 'abilities', conn, if_exists='append', index=False)
